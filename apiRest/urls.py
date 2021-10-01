9800cfeaf974719726b8500bb2775a198966ed56
Olivier Sarguini <sarguini.olivier@gmail.com>
2021-10-01 15:40:45 +0200

    reset path

--- apiRest/urls.py
+++ apiRest/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('meteor/', include('serializator.urls')),
+    path('', include('serializator.urls')),
 ]
